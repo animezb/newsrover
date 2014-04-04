2476a96825f8e4d2d35a621ff781f095f82023fd
miyachan <droidtools@gmail.com>
2014-04-04 12:43:37 -0700

    Parse multiple date formats, fixes #1

--- article.go
+++ article.go
@@ -5,6 +5,22 @@ import (
 	"time"
 )
 
+var dateFormats []string
+
+func init() {
+	dateFormats = []string{
+		"02 Jan 2006 15:04:05 MST",
+		time.RFC1123Z,
+		time.RFC1123,
+		time.RFC3339,
+		time.RFC3339Nano,
+		time.RFC850,
+		time.UnixDate,
+		time.ANSIC,
+		time.RubyDate,
+	}
+}
+
 type Group struct {
 	Name   string
 	Number int
@@ -23,8 +39,12 @@ type Article struct {
 }
 
 func (a Article) Time() time.Time {
-	t, _ := time.Parse("02 Jan 2006 15:04:05 MST", a.Date)
-	return t
+	for _, df := range dateFormats {
+		if t, err := time.Parse(df, a.Date); err == nil {
+			return t
+		}
+	}
+	return time.Time{}
 }
 
 func (a Article) Pretty() string {
